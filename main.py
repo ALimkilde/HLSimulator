ab6db7a46fbcbe06f39c12d6dff68ce7d44bed5e
Asger Limkilde <al@ticra.com>
2026-07-03 16:41:20 +0200

    Implement solver for finding the static position
    Using fsolve and an initial guess based on sag required for a specific
    tension.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import numpy as np
-from scipy.integrate import solve_ivp, RK45
+from scipy.integrate import solve_ivp
+from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import time
 from numba import njit
+import sys
 
+m_highliner = 89
 N = 41       # Discretization
 L = 100      # Line length [m]
-rho = 0.100  # Density [kg/m] (main + backup) - Joker + mamba?
+rho = 0.1    # Density [kg/m] (main + backup) - Joker + mamba?
 # rho = 0.066  # Density [kg/m] (main + backup) - Y2K
-# rho = 0.001
+# rho = 0.0001
 kl = 130*1E3 # Spring constant times length - Joker
 # kl = 500*1E3 # Spring constant times length - Y2K
 l = L/(N-1)  # length of discretized line segment
@@ -24,8 +27,41 @@ def add_tension(w):
     alpha = w/L
     l = l*(1+alpha)
 
-def animate_rope(result, skip=50):
-    fig, ax = plt.subplots()
+def compute_tension(pos):
+    t = np.zeros(2*N-2)
+
+    for i in range(0,2*N-2,2):
+        zi = np.array([pos[i], pos[i+1]])
+        zip1 = np.array([pos[i+2], pos[i+3]])
+        t[i] = tension(zi, zip1)
+
+    return np.max(t)
+
+
+def plot_rope(Z,ax = None, label = None):
+    if (ax is None):
+       fig, ax = plt.subplots(figsize=(16, 9))
+
+    ax.set_xlim(-0.1 * L, 1.1 * L)
+    ax.set_ylim(-0.2 * L, 0.2 * L)
+    ax.set_aspect('equal')
+    ax.grid(True)
+
+    if (label is None):
+        label = 'Line'
+
+
+    x = Z[0:2*N:2]
+    y = Z[1:2*N:2]
+
+    ax.plot(x, y, 'o-', lw=2, ms=4, label = label)
+    ax.legend()
+
+    return ax
+
+
+def animate_rope(result, skip=20):
+    fig, ax = plt.subplots(figsize=(16, 9))
 
     ax.set_xlim(-0.1 * L, 1.1 * L)
     ax.set_ylim(-0.2*L, 0.2*L)
@@ -85,7 +121,7 @@ def net_force(zim1, zi, zip1, mm):
     return F
 
 @njit
-def rhs(t, Z):
+def rhs_t(t, Z):
     out = np.zeros(4*N)
 
     out[0:2*N] = Z[2*N:]
@@ -94,11 +130,10 @@ def rhs(t, Z):
         if (i < 2 or i >= 2*N-2): continue
 
         if (i == N-1): 
-            mm = 80
+            mm = m + m_highliner
         else:
             mm = m
         
-
         zi = np.array([Z[i], Z[i+1]])
         zim1 = np.array([Z[i-2], Z[i-1]])
         zip1 = np.array([Z[i+2], Z[i+3]])
@@ -109,27 +144,95 @@ def rhs(t, Z):
 
     return out
 
-def main():
-    x = np.linspace(0, L, N)
-    y = np.zeros_like(x) 
+@njit
+def rhs(Z):
+    out = np.zeros(2*N)
 
-    add_tension(-2)
-    
+    out[0] = Z[0]             # x0 = 0
+    out[1] = Z[1]             # y0 = 0
+    out[2*N-2] = Z[2*N-2] - L # x1 = L
+    out[2*N-1] = Z[2*N-1]     # y1 = 0
+
+    for i in range(0,2*N,2):
+        if (i < 2 or i >= 2*N-2): continue
+
+        if (i == N-1): 
+            mm = m + m_highliner
+        else:
+            mm = m
+        
+        zi = np.array([Z[i], Z[i+1]])
+        zim1 = np.array([Z[i-2], Z[i-1]])
+        zip1 = np.array([Z[i+2], Z[i+3]])
+
+        F = net_force(zim1, zi, zip1, mm)
+        out[i] = F[0]/mm   
+        out[i+1] = F[1]/mm 
+
+    return out
+
+# Initial guess assuming that with weight of the highliner and line in the middle
+# the line will sag to yield a tension of T_kN
+def get_initial_from_tension(T_kN = 2):
+    T_kg = 1000.0/9.82 * T_kN
+    mass = m_highliner + rho*L
+    s = mass * L / (4*T_kg)
+    a = -2*s/L
+
+    x = np.linspace(0, L, N)
+    y = np.maximum(x*a, (L-x)*a) 
+
     positions = np.column_stack((x, y)).ravel()
     velocities = np.zeros_like(positions)
     
     Z = np.concatenate((positions, velocities))
 
-    tmp = rhs(0,Z)
+    return Z
 
-    t0 = 0
-    t1 = 30
+def get_static_position(pos):
+    sol, info, ier, mesg = fsolve(rhs, pos, full_output=True)
+    # print("Solution:", sol)
+    print("ier:", ier)
+    print("Message:", mesg)
+    return sol
 
-    result = solve_ivp(rhs, (t0,t1), Z)
 
-    print(f"lowest point: {np.min(result.y[1:2*N:2,-1])}")
+def main():
 
-    animate_rope(result)
+    add_tension(0)
+    
+    Z = get_initial_from_tension(T_kN = 3)
+    pos = Z[0:2*N]
+    vel = Z[2*N:]
+
+    ax = plot_rope(Z)
+    plt.title(f"Initial Guess")
+
+    pos = get_static_position(pos)
+    Z = np.concatenate((pos, vel))
+    
+    plot_rope(Z, ax = ax, label = 'static pos')
+    plt.title(f"Tension = {compute_tension(pos)/1000} kN")
+    print(f"lowest point: {np.min(pos[1:2*N:2])}")
+    print(f"weight of line: {rho*L}")
+    plt.show()
+
+    # t0 = 0
+    # t1 = 50
+
+    # result = solve_ivp(rhs_t, (t0,t1), Z)
+    # animate_rope(result)
+    # Z = result.y[:,-1]
+    # plot_rope(Z)
+
+    # t0 = 0
+    # t1 = 20
+
+    # result = solve_ivp(rhs_t, (t0,t1), Z)
+
+    # print(f"lowest point: {np.min(result.y[1:2*N:2,-1])}")
+
+    # animate_rope(result)
 
 if __name__ == "__main__":
     main()
