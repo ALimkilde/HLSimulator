2f0a0299e1ce02f7e655e9cce82ec667ea6421b9
Asger Limkilde <al@ticra.com>
2026-07-07 17:41:38 +0200

    Cleanup webbings and prepare for segmented setup

--- config.py
+++ config.py
@@ -1,3 +1,57 @@
+from dataclasses import dataclass
+from functools import cached_property
+
+@dataclass
+class webbing:
+    stretch_pct : float # Stretch percentage at the tension
+    tension_kN  : float # The tension for which the stretch is given in [kN]
+    weight_g_m  : float # Weight in [g/m]
+
+    @cached_property
+    def rho(self):
+        return self.weight_g_m/1000 # switch to SI unit [kg]
+
+    @cached_property
+    def kl(self):
+        return 100*self.tension_kN/self.stretch_pct * 1E3
+
+
+@dataclass
+class segment:
+    kl_main    : float # Spring constant times length
+    kl_backup  : float # Spring constant times length
+    rho_main   : float # Density of main
+    rho_backup : float # Density of backup
+    L_main     : float # Length of main piece
+    L_backup   : float # Length of backup piece
+
+    def __init__(
+        self,
+        webbing_main: webbing,
+        webbing_backup: webbing,
+        L_main: float,
+        L_backup: float,
+    ):
+        self.kl_main = webbing_main.kl
+        self.kl_backup = webbing_backup.kl
+        self.rho_main = webbing_main.rho
+        self.rho_backup = webbing_backup.rho
+        self.L_main = L_main
+        self.L_backup = L_backup
+
+# Webbings
+joker = webbing(stretch_pct = 3.6,  tension_kN = 5, weight_g_m = 54) 
+solid = webbing(stretch_pct = 2.5,  tension_kN = 5, weight_g_m = 50) 
+y2k   = webbing(stretch_pct = 1.0,  tension_kN = 5, weight_g_m = 33) 
+
+# Webbings and line
+L = 150             # Line length [m]
+L_backup = L + 20
+pull_webbing = -2.0
+
+# Segmented setup
+segs = [ segment(joker, solid, L, L_backup) ]
+
 # Experiment
 break_mainline = False
 
@@ -6,22 +60,13 @@ m_slackliner = 89  # Mass if slackliner [kg]
 l_leash = 1.3      # Length of leash [m]
 l_leg = 1.1        # Length of legs [m] (until harness connection point)
 
-# Webbings and line
-# length
-L = 150             # Line length [m]
-L_backup = L + 20
-pull_webbing = -2.0
-
 # weight
-rho = 0.055        # Density [kg/m] (main) - Joker 
-rho_backup = 0.050 # Density [kg/m] (main) - Mamba
-# rho = 0.033      # Density [kg/m] (main) - Y2K
-# rho_backup = 0.033      # Density [kg/m] (backup) - Y2K
+rho = segs[0].rho_main        # Density [kg/m] (main) - Joker 
+rho_backup = segs[0].rho_backup # Density [kg/m] (main) - Mamba
 
 # elasticity
-kl = 139*1E3       # Spring constant times length - Joker
-kl_backup = 200*1E3       # Spring constant times length - Solid
-# kl = 500*1E3     # Spring constant times length - Y2K
+kl = segs[0].kl_main            
+kl_backup = segs[0].kl_backup
 kl_leash = 200*1E3 # Spring constant times length - Leash
 
 # Discretization
